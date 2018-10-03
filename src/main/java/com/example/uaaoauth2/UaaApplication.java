cf095f51e4a71a31b9d73654e8512f841dc5c4de
d.kanehira <disc99.o@gmail.com>
2018-10-03 10:14:49 +0900

    Initialize project

---
+++ src/main/java/com/example/uaaoauth2/UaaApplication.java
@@ -0,0 +1,12 @@
+package com.example.uaaoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UaaApplication.class, args);
+	}
+}
